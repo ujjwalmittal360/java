817c884c346ed4cfc9164e4748cb969caeaf3a45
ujjwalmittal360 <ujjwalmittal360@gmail.com>
2026-04-13 18:45:40 +0530

    first commit

---
+++ CoreJava/RelationalOperators.java
@@ -0,0 +1,20 @@
+class RelationalOperators{
+    public static void main(String arg[]){
+
+        int a=5;
+        int b=6;
+
+        // generally there are only 3 relation but we can make more using comnination of these 3
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+
+        // other relation 
+
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+
+
+    }
+}
